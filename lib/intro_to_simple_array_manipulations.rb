43c2513dbd7d62cd47b187a087681dc023d4b375
John Lonetree <jlonetree@gmail.com>
2020-08-08 23:45:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_concat(my_favorite_things, all_my_favs)
 end
 
 def using_insert(list_of_programming_languages)
-  list_of_programming_languages.insert
+  list_of_programming_languages.insert(4, "python")
 end
 
 def using_uniq
