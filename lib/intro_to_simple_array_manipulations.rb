570791369f527aab0359cdf434fc600abd8e09d2
John Lonetree <jlonetree@gmail.com>
2020-08-09 00:09:12 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, all_my_favs)
+  my_favorite_things.concat(all_my_favs)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
+end
+
+def using_delete_at(famous_robots, deleted_robots)
+  famous_robots.delete_at(2)
+end
