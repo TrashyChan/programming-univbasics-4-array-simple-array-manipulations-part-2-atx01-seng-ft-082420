fa7eff4246962d585e7a406e408bdf739610caa7
John Lonetree <jlonetree@gmail.com>
2020-08-09 00:06:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(my_favorite_things, all_my_favs)
   my_favorite_things.concat(all_my_favs)
 end
 
-def using_insert(array)
+def using_insert(array,element)
   array.insert(3, element)
 end
 
